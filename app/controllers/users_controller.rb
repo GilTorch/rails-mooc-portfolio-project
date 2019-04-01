cf7a9c199ebb01ee3fe66d16012e94014fcfd98c
GilTorch <infotorch2014@gmail.com>
2019-04-01 14:32:06 -0400

    user can sign up via a form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def new 
+        @user=User.new
+    end
+
+    def create 
+        @user=User.new(user_params)
+        if @user.save
+            redirect_to "/login"
+        else 
+            redirect_to "/users/new"
+        end
+    end
+
+
+    def user_params
+        params.require(:user).permit(:username,:email,:password,:password_confirmation)
+    end
+
+
+
 end
