fb68db9aae4582f620aed1bf0cc806945ed4591e
GilTorch <infotorch@gmail.com>
2019-04-02 14:36:41 -0400

    new validation added for course: title should be unique and cannot be empty

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord
+    validates :title,presence: true, uniqueness: true
     has_many :user_courses 
     has_many :users, through: :user_courses 
     has_many :chapters
