9f79b7a9d2c1f552719a7e41970df197bc3c1346
GilTorch <infotorch@gmail.com>
2019-04-02 14:09:40 -0400

    more logical authentication flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,31 +3,33 @@ class SessionsController < ApplicationController
 
     def create
         if auth
-            @user = User.find_or_create_by(id: auth['uid']) do |u|
+            user = User.find_or_create_by(id: auth['uid']) do |u|
                 u.username = auth['info']['name']
                 u.email = auth['info']['email']
                 # u.image = auth['info']['image']
               end
-              session[:username]=@user.username 
-              redirect_to "/"
+              session[:username]=user.username 
+              redirect_to user_path(user)
         else
             user=User.find_by(username:params[:username])
             if user && user.authenticate(params[:password])
-            session[:username]=user.username 
-            redirect_to "/"
+                session[:username]=user.username 
+                redirect_to user_path(user)
+            else 
+                redirect_to "/"
             end
         end
     end 
 
-    def facebook_auth 
-        puts params.inspect
-    end
 
     def new 
+        if is_logged_in?
+            redirect_to user_path(current_user.id)
+        end
     end 
 
     def destroy 
-      if session.key?("username")
+      if is_logged_in?
         session.delete :username
         redirect_to "/"
       end
@@ -36,5 +38,13 @@ class SessionsController < ApplicationController
     private 
         def auth 
             request.env["omniauth.auth"]
+        end
+
+        def is_logged_in?
+            session[:username]!=nil
+        end
+    
+        def current_user 
+            User.find_by(username:session[:username])
         end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,18 @@ class UsersController < ApplicationController
     def create 
         @user=User.new(user_params)
         if @user.save
-            redirect_to "/login"
+            session[:username]=@user.username
+            redirect_to user_path(@user)
         else 
             redirect_to "/users/new"
         end
     end
     
-    def show 
-        @user=User.find_by(params[:id])
+    def show
+        @user=User.find_by(id:params[:id])
+        if !@user 
+            redirect_to "/"
+        end
     end
 
 
