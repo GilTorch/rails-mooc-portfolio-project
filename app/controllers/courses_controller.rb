63d95cd8bc5bd1c89156fc4e030a63fecd2e68d2
GilTorch <infotorch@gmail.com>
2019-04-02 14:47:45 -0400

    basic controller methods (create,show,new) added for courses. Also created 'root' route helper as an alias

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,24 @@
 class CoursesController < ApplicationController
+    def new 
+        @course=Course.new
+    end
+
+    def create
+        course=Course.new(course_params)
+
+        if course.save 
+            redirect_to course_path(course)
+        else 
+            redirect_to root_path
+        end
+    end
+
+    def show 
+        @course=Course.find_by(id:params[:id])
+    end
+
+
+    def course_params
+        params.require(:course).permit(:title)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get "/auth/facebook/callback",to:"sessions#create"
 
-  get "/",to:"application#home"
+  get "/",to:"application#home",as:"root"
 
   resources :lessons
   resources :chapters
