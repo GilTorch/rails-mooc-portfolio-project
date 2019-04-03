9399ede9131eced42ccdfb5877d08e70d284678c
GilTorch <infotorch@gmail.com>
2019-04-03 12:59:54 -0400

    new validations

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,4 +1,5 @@
 class Chapter < ApplicationRecord
+    validates :title,presence: true
     has_many :user_chapters 
     has_many :users, through: :user_chapters 
     belongs_to :course
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
 class Lesson < ApplicationRecord
+    validates :title,presence: true
     has_many :user_lessons 
     has_many :users, through: :user_lessons 
     belongs_to :chapter
