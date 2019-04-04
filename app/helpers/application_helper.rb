601446ce0f9ea9ffaa3e8dc36e46dad1db362a9b
GilTorch <infotorch@gmail.com>
2019-04-04 12:59:04 -0400

    chapters can now be completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,12 +11,23 @@ module ApplicationHelper
     end
 
     def lesson_completed(lesson)
-        puts current_user.inspect
         user_lesson= UserLesson.find_by(lesson_id:lesson.id,user_id:current_user.id)
         if user_lesson 
             user_lesson.completed
         else 
             false
         end
+    end
+
+    def chapter_completed(chapter)
+        user_chapter=UserChapter.find_by(chapter_id:chapter.id,user_id:current_user.id)
+        if !chapter.lessons.empty? && chapter.lessons.each do |lesson|
+                user_lesson=UserLesson.find_or_create_by(lesson_id:lesson.id)
+                if !user_lesson.completed
+                    return false
+                end
+            end
+        end
+        return true
     end
 end
