abd2860ee91199b5faa4e0ec24b8f1c483830853
GilTorch <infotorch@gmail.com>
2019-04-04 09:00:59 -0400

    WIP: fixing current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
     def home 
     end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,19 @@ module ApplicationHelper
     end
 
     def current_user 
-        User.find_by(username:session[:username])
+        if is_logged_in?
+           user= User.find_by(username:session[:username])
+        #    puts User.all.inspect
+        end 
+    end
+
+    def lesson_completed(lesson)
+        puts current_user.inspect
+        user_lesson= UserLesson.find_by(lesson_id:lesson.id,user_id:current_user.id)
+        if user_lesson 
+            user_lesson.completed
+        else 
+            false
+        end
     end
 end
