6a0e8a475180cccef0925a351a64c2ee012e8302
GilTorch <infotorch@gmail.com>
2019-04-08 12:25:55 -0400

    basic UI created for auth forms. Font awesome support handled.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,29 @@
 class SessionsController < ApplicationController
     skip_before_action :verify_authenticity_token  
 
+
     def create
         if auth
             
             user = User.find_or_create_by(id: auth['uid']) do |u|
                 u.username = auth['info']['name']
                 u.email = auth['info']['email']
-                u.password_digest="No Password"
+                real_password="password"
+                salt="my_salt"
+                sha1_password=Digest::SHA1.hexdigest("#{salt}#{real_password}")
+                u.password_digest=BCrypt::Password.create(sha1_password).to_s
                 # u.image = auth['info']['image']
               end
-              puts user.inspect
               session[:username]=user.username 
               redirect_to user_path(user)
         else
             user=User.find_by(username:params[:username])
+
             if user && user.authenticate(params[:password])
                 session[:username]=user.username 
                 redirect_to user_path(user)
             else 
-                redirect_to "/"
+                redirect_to root_path
             end
         end
     end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # authentication
   get "/login",to:"sessions#new"
   post "/login",to:"sessions#create"
-  post "/logout",to:"sessions#destroy"
+  post "/logout",to:"sessions#destroy",as:"logout"
 
   get "/auth/facebook/callback",to:"sessions#create"
 
