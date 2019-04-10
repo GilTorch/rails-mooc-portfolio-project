84059f6cc9baac273df4343949b57d194bd46f98
GilTorch <infotorch@gmail.com>
2019-04-10 11:01:02 -0400

    Fixed merge conflicts

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,4 +1,7 @@
 class ChaptersController < ApplicationController
+    before_action :require_login 
+    before_action :set_chapter,only:[:edit,:show,:update,:reset,:destroy]
+
     def index
         @chapters=Chapter.all
     end
@@ -19,18 +22,16 @@ class ChaptersController < ApplicationController
     end
 
     def edit
-        @chapter=Chapter.find_by(id:params[:id])
         @courses=Course.all
     end
 
     def update 
-        puts params.inspect
-        @chapter=Chapter.find_by(id:params[:id])
         @chapter.course=Course.find_by(id:params[:course_id])
         puts @chapter.inspect
         if @chapter.update(chapter_params)
             redirect_to chapter_path(@chapter) 
         else 
+            flash[:danger]="The chapter you are trying to "
             redirect_to root_path
         end
  
@@ -38,17 +39,23 @@ class ChaptersController < ApplicationController
 
 
     def destroy
-        chapter=Chapter.find_by(id:params[:id])
         chapter.destroy 
         redirect_to chapters_path
     end
 
 
     def show 
-        @chapter=Chapter.find_by(id:params[:id])
     end
 
     def chapter_params 
         params.require(:chapter).permit(:title)
+    end
+
+    def set_chapter 
+       @chapter=Chapter.find_by(id:params[:id])
+    end
+
+    def require_login
+        redirect_to login_path unless session.include? :username
     end
 end
