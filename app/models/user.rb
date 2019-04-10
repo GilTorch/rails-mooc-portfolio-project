9b837ccd22256a043d8563e5e55d1551a404e7a1
GilTorch <infotorch@gmail.com>
2019-04-10 10:02:48 -0400

    showing validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     
     validates :email,presence: true, uniqueness: true 
     validates :username,presence: true, uniqueness: true 
-
+    validates :password,length:{ minimum: 8}, confirmation: { case_sensitive: true}
 
     has_many :user_courses 
     has_many :courses, through: :user_courses 
